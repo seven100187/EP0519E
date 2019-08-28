394d2d772cc8e7b9313326cfe28e4ff4e9cd9c6e
seven1001 <khotv1001@gmail.com.com>
2019-08-28 20:39:02 +0700

    Add code example for pit module and software timer
    Signed-off-by: seven1001 <khotv1001@gmail.com.com>

--- EP0519E_WS/Mcg_project/source/main.c
+++ EP0519E_WS/Mcg_project/source/main.c
@@ -1,7 +1,7 @@
 #include <MKL46Z4.h>
 
 #define LED (1<<29)
-#define COUNTER (8000 - 1)
+#define COUNTER (20000 - 1)
 
 uint32_t tick = 0;
 
@@ -59,11 +59,58 @@ void FEE_8Mhz()
   SIM->CLKDIV1 |= (uint32_t)SIM_CLKDIV1_OUTDIV1(2);
 }
 
+void FEI_to_FBE()
+{
+  MCG->C1 &= ~(uint32_t)MCG_C1_CLKS_MASK;
+  MCG->C1 |= (uint32_t)MCG_C1_CLKS(2);
+  
+  MCG->C1 &= ~(uint32_t)MCG_C1_IREFS_MASK;
+  
+  MCG->C2 |= (uint32_t)MCG_C2_RANGE0(2);
+  
+  MCG->C1 &= ~(uint32_t)MCG_C1_FRDIV_MASK;
+  MCG->C1 |= (uint32_t)MCG_C1_FRDIV(3);
+  
+  MCG->C4 &= ~(uint32_t)MCG_C4_DMX32_MASK;
+  MCG->C4 &= ~(uint32_t)MCG_C4_DRST_DRS_MASK;
+  
+  MCG->C6 &= ~(uint32_t)MCG_C6_PLLS_MASK;
+  MCG->C2 &= ~(uint32_t)MCG_C2_LP_MASK;
+  
+  SIM->CLKDIV1 &= ~(uint32_t)SIM_CLKDIV1_OUTDIV1_MASK;
+  SIM->CLKDIV1 |= (uint32_t)SIM_CLKDIV1_OUTDIV1(0);
+}
+
+void FBE_to_PBE()
+{  
+  MCG->C5 &=~(uint32_t)MCG_C5_PRDIV0_MASK;
+  MCG->C5 |= (uint32_t)MCG_C5_PRDIV0(4);
+  
+  MCG->C6 &= ~(uint32_t)MCG_C6_VDIV0_MASK;
+  
+  MCG->C6 &= ~(uint32_t)MCG_C6_PLLS_MASK;
+  MCG->C6 |= (uint32_t)MCG_C6_PLLS_MASK;
+  
+  MCG->C2 &= ~(uint32_t)MCG_C2_LP_MASK;
+}
+
+void FBE_to_PEE()
+{
+  MCG->C1 &= ~(uint32_t)MCG_C1_CLKS_MASK;
+  SIM->CLKDIV1 &= ~(uint32_t)SIM_CLKDIV1_OUTDIV1_MASK;
+  SIM->CLKDIV1 |= (uint32_t)SIM_CLKDIV1_OUTDIV1(0);
+}
+
+
 int main()
 {
   init_led();
-  FEI_8Mhz();
   init_systick();
+  FEI_to_FBE();
+  
+  delay_ms(500);
+  //FBE_to_PBE();
+  //FBE_to_PEE();
   while(1)
   {
     GPIOE->PTOR |= (uint32_t)LED;
